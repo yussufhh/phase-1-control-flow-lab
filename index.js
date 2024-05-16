4c19467f400c39821bc0c45038cc523ef20b43f0
yussufhh <0793477632g@gmail.com>
2024-05-16 17:57:58 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <=400){
+    return 'This one is on me!'
+  }
+ 
+     else if (ride > 2000 && ride <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else if (ride > 2500) {
+      return 'No can do.';
+    }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+        default:
+          return 'Bye.'
+  }
+ 
 }
